848df5dce3acb2d1754bd1bb7152c0eed8142003
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-01-04 23:41:50 +0000

    add teacher table and content

--- 3_queries_group-by/large_cohorts.sql
+++ 3_queries_group-by/large_cohorts.sql
@@ -4,3 +4,34 @@ JOIN students ON cohorts.id = cohort_id
 GROUP BY cohort_name 
 HAVING count(students.*) >= 18
 ORDER BY student_count;
+
+-- -- Why does this not work?
+-- SELECT count(assignments)-count(assignment_submissions) as total_incomplete
+-- FROM assignment_submissions
+-- -- 
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
+
+-- SELECT (
+--   SELECT count(assignments)
+--   FROM assignments
+-- )-count(assignment_submissions) as total_incomplete
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
+
+-- SELECT count(students)
+-- FROM students
+-- -- What is ""= cohort_id"?
+-- JOIN cohorts on cohorts.id = cohort_id
+-- GROUP BY cohorts;
+
+-- SELECT assignments.name
+-- FROM assignments 
+-- WHERE id NOT IN
+-- (
+--   SELECT assignment_id
+--   FROM assignment_submissions
+--   JOIN students ON students.id = student_id
+--   WHERE students.name = 'Ibrahim Schimmel'
+-- );
--- 3_queries_group-by/total_assignment_submissions.sql
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -1,6 +1,28 @@
 SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
 FROM assignment_submissions
+-- connects pk to fk
 JOIN students ON students.id = student_id
+-- connects pk to fk
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY total_submissions DESC;
+
+-- -- for each cohort
+-- SELECT cohorts.name AS cohort, (
+--   -- count assignment submissions based on id column
+--   SELECT count(assignment_submissions.id)
+--   -- from the assignment submissions table
+--   FROM assignment_submissions
+--   -- compare current cohort with the cohort id of the assignment submission
+--   WHERE cohorts.id = (
+--     -- assignment sub doesnt save cohort id column so it needs to be specified 
+--     SELECT cohort_id
+--     FROM students
+--     WHERE students.id = assignment_submissions.student_id
+--     -- ensure sub quierie returns 1 record
+--     LIMIT 1
+--   )
+-- ) AS total_submissions
+-- FROM cohorts
+-- GROUP BY cohorts.id
+-- ORDER BY total_submissions DESC;
